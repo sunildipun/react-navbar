3317e3aa919cbc98f666cc1fec3ef920a9d67105
sunildipun <sunil.6318ie@gmail.com>
2021-01-08 19:30:55 +0530

    nav bar css fixes

--- src/component/navbar/navbar.component.jsx
+++ src/component/navbar/navbar.component.jsx
@@ -2,11 +2,49 @@ import React from 'react';
 
 import './navbar.style.css';
 
+import { links, social } from './../../data';
+
+import {ReactComponent as Logo} from  './../../logo.svg';
+import { FaBars } from 'react-icons/fa';
+
+
+import './navbar.style.css';
+
 const Navbar = () => {
     return (
-        <div>
-            This is navbar
-        </div>
+        <nav>
+            <div className='nav-center'>
+                <div className='nav-header'>
+                {/** Logo Svg */}
+                <Logo  className="logo"/>
+                <button className='nav-toggle'>
+                    <FaBars />
+                </button>
+                </div>
+                <div className='links-container' >
+                    <ul className='links' >
+                        {links.map((link) => {
+                        const { id, url, text } = link;
+                        return (
+                            <li key={id}>
+                            <a href={url}>{text}</a>
+                            </li>
+                        );
+                        })}
+                    </ul>
+                </div>
+                <ul className='social-icons'>
+                {social.map((socialIcon) => {
+                    const { id, url, icon } = socialIcon;
+                    return (
+                    <li key={id}>
+                        <a href={url}>{icon}</a>
+                    </li>
+                    );
+                })}
+                </ul>
+            </div>
+        </nav>
     )
 }
 
